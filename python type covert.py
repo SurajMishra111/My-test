11eb8fe9bc2e699434a5752544c2bd0cbf88aa5f
SurajMishra111 <111695725+SurajMishra111@users.noreply.github.com>
2022-08-21 08:06:59 -0700

    Add files via upload

---
+++ python type covert.py
@@ -0,0 +1,30 @@
+1. Write a python script to convert a number into str type.
+x=25
+print(type(x))
+y=str(x)
+print(type(y))
+2.Write a python script to print Unicode of the character ‘m’
+print(ord('m'))
+3.Write a python script to print character representation of a given unicode 100.
+print(chr(100))
+4. Write a python script to print any number and its binary equivalent
+print(bin(25))
+5.Write a python script to print any number and its octal equivalent.
+print(oct(25))
+6.Write a python script to store binary number 1100101 in a variable and print it in
+decimal format.
+x=0b1100101
+print(x)
+7.Write a python script to store a hexadecimal number 2F in a variable and print it in
+octal format.
+x=0x2F
+print(oct(x))
+8.Write a python script to store an octal number 125 in a variable and print it in binary
+format.
+x=0o125
+print(bin(x))
+10. Write a python script to add two numbers 25 (in octal) and 39 (in hexadecimal) and
+display the result in binary format.
+x=0o25
+y=0x39
+print(bin(x+y))
